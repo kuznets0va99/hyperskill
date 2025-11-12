eafed9e6782d1dd1ca54009d782f38bf3601a870
kuznets0va99 <k3.ol@yandex.ru>
2025-11-13 01:46:53 +0300

    Update Main.java

--- Operations on primitive types/task_5/Main.java
+++ Operations on primitive types/task_5/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int n = scanner.nextInt();
+        int tensDigit = (n / 10) % 10;
+        System.out.println(tensDigit);
+        scanner.close();
     }
 }
