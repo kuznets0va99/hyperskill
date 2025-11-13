5867759dac7e6540f98485c316e8458053835b5f
kuznets0va99 <k3.ol@yandex.ru>
2025-11-14 02:22:38 +0300

    new

---
+++ Difference between two doubles/task_2/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        x = Math.pow(x, 3) + Math.pow(x, 2) + x + 1;
+        System.out.println(x);
+        scanner.close();
+    }
+}
