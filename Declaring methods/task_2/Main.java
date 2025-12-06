3837f6c8cd9785adcec0647beaca11a742aefe4e
kuznets0va99 <k3.ol@yandex.ru>
2025-12-06 20:48:29 +0300

    new

--- Declaring methods/task_2/Main.java
+++ Declaring methods/task_2/Main.java
@@ -1,16 +1,32 @@
+package task_2;
 import java.util.Scanner;
 
 public class Main {
 
-    public static double divide(long a, long b) {
-        return (double) a / b;
+    /**
+     * The method calculates the sum of integers in a given range
+     *
+     * @param from inclusive
+     * @param to exclusive
+     *
+     * @return the sum (long)
+     */
+    public static long sumInRange(int from, int to) {
+        // write your code here
+        long res = 0;
+        for (int i = from; i < to; i++){
+            res += i;
+        }
+        return (res);
     }
 
     /* Do not change code below */
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        final long a = scanner.nextLong();
-        final long b = scanner.nextLong();
-        System.out.println(divide(a, b));
+
+        int from = scanner.nextInt();
+        int to = scanner.nextInt();
+
+        System.out.println(sumInRange(from, to));
     }
 }
